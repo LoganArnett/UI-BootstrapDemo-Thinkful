78e18e67902f3e8169083636d32616c20818e360
LoganArnett <Loganarn5@gmail.com>
2015-10-15 09:11:42 -0400

    Basic Angular setup for the app :octocat:

---
+++ app/controllers/main.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('MyBootstrApp') // declare which module this controller should attach to
+
+.controller('MainCtrl', function($scope){ // create our Main Controller
+  $scope.demo = "AngularUI Bootstrap"
+})
